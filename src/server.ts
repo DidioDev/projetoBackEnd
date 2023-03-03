7d83ccf73024226a716d62c3955ae09ac563d2c9
DidioDev <lucassmaniottoo@gmail.com>
2023-03-03 20:11:05 -0300

    Second commit

--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 
-const server = express()
-const port = 3000
+const server = express();
+const port = 3000;
 
 server.get("/", (request, response) => {
     response.send('Hello World!')
 });
 
+server.post('/', (request, response) => {
+
+});
+
 server.listen(port, () => {
-    console.log("Server Running!! 🎲");
-    
+    console.log(`Server Running - end: http://localhost:${port}`)
 });
